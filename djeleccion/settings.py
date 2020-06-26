806b9bfdd6ee235f5a183e603461eee249de143e
Hector_santiago <53335831+Hector981@users.noreply.github.com>
2020-06-26 12:47:08 -0500

    Update settings.py

--- djeleccion/settings.py
+++ djeleccion/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '$yj1bwkj+0wfy8k*j-3@j(!4r_-e-su-#*#atdr04tu5@*sg&g'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
